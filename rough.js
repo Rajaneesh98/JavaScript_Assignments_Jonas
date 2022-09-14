3fc2d4b3f9c1b0cf8fa8ddb89eb030797c3e1224
Rajaneesh <rajaneesh1998@gmail.com>
2022-09-14 19:29:06 +0530

    switch case

--- rough.js
+++
@@ -1,3 +0,0 @@
-let year = "2022";
-console.log(3 + '3' -3 - '3');
-console.log(2 ** 3);
---
+++ switchStatements.js
@@ -0,0 +1,30 @@
+let day = 6;
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+
+  case 2:
+    console.log("tuesday");
+    break;
+
+  case 3:
+    console.log("wednesday");
+    break;
+
+  case 4:
+    console.log("Thursday");
+
+  case 5:
+    console.log("friday");
+    break;
+
+  case 6:
+  case 7:
+    console.log("weekend");
+    break;
+
+  default:
+    console.log("enter valid day");
+}
